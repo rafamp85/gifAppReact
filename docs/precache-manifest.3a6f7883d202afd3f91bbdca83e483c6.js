065c749cb36b3de4205f523ec191dc0b58e8ffff
Rafael Morales <rafaelmp@gmail.com>
2020-08-21 11:02:51 -0500

    Modifying build by docs

---
+++ docs/precache-manifest.3a6f7883d202afd3f91bbdca83e483c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7129362a4ea15b71d9ed65cbec68def7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e1b24f1729d55257d05",
+    "url": "/static/css/main.09203e70.chunk.css"
+  },
+  {
+    "revision": "100caeaeffd01e7cd693",
+    "url": "/static/js/2.78557d1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78557d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1b24f1729d55257d05",
+    "url": "/static/js/main.7fefad6d.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
