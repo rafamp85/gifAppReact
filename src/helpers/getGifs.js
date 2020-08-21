8a4cc923e77ffe451d4b5acd7eafed36567f13ef
Rafael Morales <rafaelmp@gmail.com>
2020-08-21 11:01:07 -0500

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,20 @@
+
+
+const key = 'ZCJKeSav9pUug2Uv44TUJ2mxQMJXYzse';
+
+export const getGifs = async( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${key}`
+
+    const resp = await fetch( url );
+    const {data} = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+
+    return gifs;
+}
